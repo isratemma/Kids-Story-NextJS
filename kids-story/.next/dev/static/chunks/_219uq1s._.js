55a4f0cbbd930f232274985d619fc35d33163daf
isratemma <emmaaissraatt@gmail.com>
2026-09-22 20:52:15 +0600

    chore: remove accidentally committed .next dev cache

--- kids-story/.next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0gizq-0._.css","static/chunks/node_modules_next_1vpm69o._.js","static/chunks/src_components_buttons_NavLInk_jsx_0yne4m6._.js","static/chunks/node_modules_react-icons_bi_index_mjs_10yly18._.js","static/chunks/node_modules_react-icons_lib_11_gx9i._.js","static/chunks/src_app_error_jsx_0r709pv._.js"],
-    source: "entry"
-});
